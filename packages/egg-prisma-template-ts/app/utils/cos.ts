432d02aff878011137f6aa737d5cd37d14070ccb
mrgaogang <gaogangwork@qq.com>
2022-12-22 19:35:44 +0800

    feat: support cos/oss file upload

--- packages/egg-prisma-template-ts/app/utils/cos.ts
+++ packages/egg-prisma-template-ts/app/utils/cos.ts
@@ -1,10 +1,13 @@
 import COS from 'cos-nodejs-sdk-v5';
 
 // create instance
-const cos = new COS({
+const cos = process.env.FILE_UPLOAD_TYPE === 'cos' ?  new COS({
     SecretId: process.env.FILE_UPLOAD_COS_ACCESS_SECRET,
     SecretKey: process.env.FILE_UPLOAD_COS_ACCESS_KEY,
-});
+}): {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    putObject(){}
+};
 
 export {
     cos
